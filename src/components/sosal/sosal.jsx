cc85bcd88efe5d5188ae86ba08c0709d5b6114b9
yeeeppppp <78420838+yeeeppppp@users.noreply.github.com>
2025-03-17 23:08:56 +0300

    пу-пу-пу....
    решить проблему с наложением бекграунда блоков, ваня помоги(

---
+++ src/components/sosal/sosal.jsx
@@ -0,0 +1,64 @@
+import './sosal.css'
+
+function sosal(){
+    return(
+        <>
+        <div className="main">
+            <div className="block-1">
+                <p>Персональные условия</p>
+                <p>для студентов</p>
+                <div className="supblock-1">
+
+                </div>
+            </div>
+
+
+            <div className="block-2">
+                <p>Оперативное решение</p>
+                <p>вопросов обучение</p>
+                <div className="supblock-2">
+
+                </div>
+            </div>
+
+
+            <div className="block-3">
+                <p>Полностью прозрачное</p>
+                <p>обучение</p>
+                <div className="supblock-3">
+
+                </div>
+            </div>
+
+
+            <div className="block-4">
+                <p>Условия расположения</p>
+                <p>офиса и автодрома</p>
+                <div className="supblock-4">
+
+                </div>
+            </div>
+
+
+            <div className="block-5">
+                <p>Опытные преподаватели</p>
+                <p>с большим стажем</p>
+                <div className="supblock-5">
+
+                </div>
+            </div>
+
+
+            <div className="block-6">
+                <p>Обучнеие</p>
+                <p>в удобном для вас формате</p>
+                <div className="supblock-6">
+                    
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default sosal;
