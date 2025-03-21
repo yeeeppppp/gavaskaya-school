218eaa7f6f288a4d08869e8fd52128284c144b17
yeeeppppp <78420838+yeeeppppp@users.noreply.github.com>
2025-03-21 20:05:43 +0300

    Изменил заголовок + добавил обработчик новых страниц

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import './App.css';
 import Sosal from './components/sosal/sosal'
+import OurTeachers from './components/OurTeachers/ourteachers';
+import FAQ from './components/FAQ/FAQ';
+
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/Sosal" element={<Sosal />} />
+          <Route path='/OurTeachers' element={<OurTeachers />}/>
+          <Route path='/FAQ' element={<FAQ />}/>
         </Routes>
       </div>
     </Router>
