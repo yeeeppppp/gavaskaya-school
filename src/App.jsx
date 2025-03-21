f685c5fdf86dd1b7e25ad093dd458a1d2e6b687b
yeeeppppp <78420838+yeeeppppp@users.noreply.github.com>
2025-03-22 02:02:24 +0300

    опа подтянули

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import './App.css';
+import Sosal from './components/sosal/sosal'
+import FormatLearning from './components/FormatLearning/FormatLearning';
+import OurTeachers from './components/OurTeachers/ourteachers';
+import FAQ from './components/FAQ/FAQ';
+import AboutUs from './components/AboutUs/AboutUs'
 
 function App() {
   return (
@@ -9,7 +14,10 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/Sosal" element={<Sosal />} /> */}
+          <Route path="/Sosal" element={<Sosal />} />
+          <Route path='/OurTeachers' element={<OurTeachers />}/>
+          <Route path='/FAQ' element={<FAQ />}/>
+          <Route path='/AboutUs' element={<AboutUs />}/>
         </Routes>
       </div>
     </Router>
