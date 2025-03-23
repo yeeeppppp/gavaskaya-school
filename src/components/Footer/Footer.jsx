170af820dea7277ca24ce7306b099cfe9ac8d9f7
yeeeppppp <78420838+yeeeppppp@users.noreply.github.com>
2025-03-23 16:54:20 +0300

    21341234

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -40,8 +40,8 @@ function Footer() {
         </div>
         <div className="group-links">
           <Link to="/error" className="disabled-link">Образование</Link>
-          <Link to="/error" className="disabled-link">Руководство и педагогический состав</Link>
-          <Link to="/error" className="disabled-link">Материально-техническое обучение и оснащенность образовательного процесса</Link>
+          <a target='_blank' href="https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2Fe96PCbs%2F0wWCNOEBWwRHAuvIICmRcqjQTS7gOhVLtOwpNCL2aYtTji3PQGTGeU%2Fcq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2F%D0%A0%D1%83%D0%BA%D0%BE%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE.%D0%94%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D0%BE%D0%B1%20%D0%BF%D0%B5%D0%B4%D0%B0%D0%B3%D0%BE%D0%B3%D0%B0%D1%85.pdf&name=%D0%A0%D1%83%D0%BA%D0%BE%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE.%D0%94%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D0%BE%D0%B1%20%D0%BF%D0%B5%D0%B4%D0%B0%D0%B3%D0%BE%D0%B3%D0%B0%D1%85.pdf&nosw=1">Руководство и педагогический состав</a>
+          <a target='_blank' href="https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2Fe96PCbs%2F0wWCNOEBWwRHAuvIICmRcqjQTS7gOhVLtOwpNCL2aYtTji3PQGTGeU%2Fcq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2F%D0%9E%D1%81%D0%BD%D0%B0%D1%89%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE%20%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%D0%B0.pdf&name=%D0%9E%D1%81%D0%BD%D0%B0%D1%89%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE%20%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%D0%B0.pdf&nosw=1">Материально-техническое обучение и оснащенность образовательного процесса</a>
           <Link to="/error" className="disabled-link">Правила приема обучающихся</Link>
           <a target='_blank' href="https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2Fe96PCbs%2F0wWCNOEBWwRHAuvIICmRcqjQTS7gOhVLtOwpNCL2aYtTji3PQGTGeU%2Fcq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2F%D0%A3%D1%81%D1%82%D0%B0%D0%B2.pdf&name=%D0%A3%D1%81%D1%82%D0%B0%D0%B2.pdf&nosw=1">Устав</a>
         </div>
