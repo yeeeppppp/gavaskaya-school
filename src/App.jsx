bdeccabcc7d6720b95ed01e77926e47779fb9f91
yeeeppppp <78420838+yeeeppppp@users.noreply.github.com>
2025-03-23 13:16:05 +0300

    убрал прошлую версию наши преимущества из appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import './App.css';
-import Sosal from './components/sosal/sosal'
 import FormatLearning from './components/FormatLearning/FormatLearning';
 import OurTeachers from './components/OurTeachers/ourteachers';
 import FAQ from './components/FAQ/FAQ';
@@ -16,7 +15,6 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/Sosal" element={<Sosal />} />
           <Route path='/OurTeachers' element={<OurTeachers />}/>
           <Route path='/FAQ' element={<FAQ />}/>
           <Route path='/AboutUs' element={<AboutUs />}/>
