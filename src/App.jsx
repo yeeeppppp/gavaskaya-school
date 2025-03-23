df91bde0965255fec788988bc931d010b2e89fcf
ivwdie <ivwdie@icloud.com>
2025-03-23 15:51:48 +0300

    hotfix v2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import Error from './components/Error/Error'
+import Zapis from './components/Zapis/Zapis'
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/error" element={<Error />} /> 
+          <Route path='/zapis' element={<Zapis/>}/>
         </Routes>
       </div>
     </Router>
