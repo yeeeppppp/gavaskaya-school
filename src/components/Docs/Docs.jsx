c9509a604982474732cc86f4d2d0c335b192c579
ivwdie <ivwdie@icloud.com>
2025-03-24 22:50:15 +0300

    докс чучут фикс

--- src/components/Docs/Docs.jsx
+++ src/components/Docs/Docs.jsx
@@ -1,4 +1,5 @@
 import './Docs.css'
+import { Link } from 'react-router-dom';
 
 function Docs(){
     return (
@@ -10,10 +11,10 @@ function Docs(){
                         <a target='_blank' href="https://docviewer.yandex.ru/?url=ya-disk-public%3A%2F%2Fe96PCbs%2F0wWCNOEBWwRHAuvIICmRcqjQTS7gOhVLtOwpNCL2aYtTji3PQGTGeU%2Fcq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2F%D0%A0%D0%B5%D0%B5%D1%81%D1%82%D1%80%D0%BE%D0%B2%D0%B0%D1%8F_%D0%B2%D1%8B%D0%BF%D0%B8%D1%81%D0%BA%D0%B0%2C_%D0%BB%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_%D0%BE%D1%82_12_03_2025.pdf&name=%D0%A0%D0%B5%D0%B5%D1%81%D1%82%D1%80%D0%BE%D0%B2%D0%B0%D1%8F_%D0%B2%D1%8B%D0%BF%D0%B8%D1%81%D0%BA%D0%B0%2C_%D0%BB%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_%D0%BE%D1%82_12_03_2025.pdf">Лицензия на обучение</a>
                     </div>
                     <div className="link-two">
-                        <a href="">Коммерческое предложение</a>
+                    <Link to="/error" className="disabled-link">Коммерческое предложение</Link>
                     </div>
                     <div className="link-three">
-                        <a href="">Заключение</a>
+                    <Link to="/error" className="disabled-link">Заключение</Link>
                     </div>
                 </div>
                 <a className='link-all' href="#footer">Все документы</a>
