88b829423f8d54c35040cc2099f56aa8c8d739d4
yeeeppppp <78420838+yeeeppppp@users.noreply.github.com>
2025-03-25 01:51:15 +0300

    допилил анимации FAQ + анимации навигатора

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,53 @@
-import './Navigation.css'
-import logo from "../../assets/img/logo.png"
+import './Navigation.css';
+import logo from '../../assets/img/logo.png';
 
-function Navigation(){
-    return(
-        <>
-        <div className="nav-bar">
-            <div className="logo-sa">
-                <img src={logo} alt="logo" />
-            </div>
-            <div className="nav-el">
-                <a href="#format">Стоимость</a>
-                <a href="#learning">Обучение</a>
-                {/* <a href="#">Акции</a> */}
-                <a href="#footer">Контакты</a>
-            </div>
+function Navigation() {
+  // Функция для плавной прокрутки к секции
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+
+  return (
+    <>
+      <div className="nav-bar">
+        <div className="logo-sa">
+          <img src={logo} alt="logo" />
         </div>
-        </>
-    )
+        <div className="nav-el">
+          <a
+            href="#format"
+            onClick={(e) => {
+              e.preventDefault(); // Отменяем стандартное поведение ссылки
+              scrollToSection('format'); // Плавная прокрутка к секции
+            }}
+          >
+            Стоимость
+          </a>
+          <a
+            href="#learning"
+            onClick={(e) => {
+              e.preventDefault();
+              scrollToSection('learning');
+            }}
+          >
+            Обучение
+          </a>
+          <a
+            href="#footer"
+            onClick={(e) => {
+              e.preventDefault();
+              scrollToSection('footer');
+            }}
+          >
+            Контакты
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navigation;
